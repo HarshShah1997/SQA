f78553959b115d29a5b3227cfa2eb7a1f3091b18
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-03-08 16:26:18 +0530

    Created rdf graph

--- createRdf.py
+++ createRdf.py
@@ -1,4 +1,29 @@
 import fredlib
-sentence = "The runtime of Pulp Fiction is 184 minutes." #write here your sentence
-filename = "myfile.rdf" #a file for storing the Fred’s rdf graph
+import rdflib.extras.cmdlineutils as cmd
+import rdflib.tools.rdf2dot as rdf2dot
+import sys
+import rdflib
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('PangoCairo', '1.0')
+
+import xdot
+from gi.repository import Gtk
+
+sentence = "The runtime of Pulp Fiction is 184 minutes." 
+filename = "myfile.rdf"
 g = fredlib.getFredGraph(fredlib.preprocessText(sentence),filename)
+
+g = rdflib.Graph()
+g.parse("myfile.rdf")
+
+output_file = open("myfile.dot", "w")
+rdf2dot.rdf2dot(g, output_file)
+output_file.close()
+
+window = xdot.DotWindow()
+window.open_file("myfile.dot")
+window.connect('delete-event', Gtk.main_quit)
+Gtk.main()
+
