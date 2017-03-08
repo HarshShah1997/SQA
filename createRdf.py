df088f4ba18b233476446ef4311f690c1bdc2712
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-03-08 16:57:26 +0530

    Added file reading

--- createRdf.py
+++ createRdf.py
@@ -11,19 +11,30 @@ gi.require_version('PangoCairo', '1.0')
 import xdot
 from gi.repository import Gtk
 
-sentence = "The runtime of Pulp Fiction is 184 minutes." 
-filename = "myfile.rdf"
-g = txt2rdf.getFredGraph(txt2rdf.preprocessText(sentence),filename)
+def main():
+    sentence = sys.stdin.read()
+    if sentence.strip() == "":
+        sentence = "Valentina gave Aldo a book by Charlie Mingus."
 
-g = rdflib.Graph()
-g.parse("myfile.rdf")
+    generate(sentence)
 
-output_file = open("myfile.dot", "w")
-rdf2dot.rdf2dot(g, output_file)
-output_file.close()
+def generate(sentence):
 
-window = xdot.DotWindow()
-window.open_file("myfile.dot")
-window.connect('delete-event', Gtk.main_quit)
-Gtk.main()
+    filename = "myfile.rdf"
+    g = txt2rdf.getFredGraph(txt2rdf.preprocessText(sentence),filename)
+
+    g = rdflib.Graph()
+    g.parse("myfile.rdf")
+
+    output_file = open("myfile.dot", "w")
+    rdf2dot.rdf2dot(g, output_file)
+    output_file.close()
+
+    window = xdot.DotWindow()
+    window.open_file("myfile.dot")
+    window.connect('delete-event', Gtk.main_quit)
+    Gtk.main()
+
+if __name__ == '__main__':
+    main()
 
